49cb63532efc183d3ed4f73d923de602e2143548
Andy <andyn11bdf@gmail.com>
2018-12-15 00:40:38 -0500

    Working on algorithm without additional data structures
    Signed-off-by: Andy <andyn11bdf@gmail.com>

--- src/IsUnique.java
+++ src/IsUnique.java
@@ -1,8 +1,18 @@
 
 //Implement an algorithm to determine if a String has all unique characters.
 // With & Without implementing additional data structures
-public class IsUnique {
-    boolean is_Unique(String s) {
 
+public class IsUnique {
+    String sequence = "A;lkjqwer,mn123kd";
+
+
+    boolean check_Unique(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            return !s.contains(sequence.charAt(i));
+
+        }
+
+        return check_Unique(sequence);
     }
 }
+
