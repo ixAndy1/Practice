44afa7098106a458330b7ac9b87669bef79224bf
Andy <andyn11bdf@gmail.com>
2018-12-14 23:13:39 -0500

    Testing :D
    Signed-off-by: Andy <andyn11bdf@gmail.com>

--- src/CtCI_Questions.java
+++ src/CtCI_Questions.java
@@ -1,5 +1,7 @@
-public class CtCI_Questions {
-    public static void main(String[] args) {
+public class CtCI_Questions
+{
+    public static void main(String[] args)
+    {
         System.out.println("Test");
     }
 }
