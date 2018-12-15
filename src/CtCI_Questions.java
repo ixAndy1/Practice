963a13929b57361d7aa5eb224390946d9b1ebead
Andy <andyn11bdf@gmail.com>
2018-12-15 00:18:41 -0500

    Setting up
    Signed-off-by: Andy <andyn11bdf@gmail.com>

--- src/CtCI_Questions.java
+++
@@ -1,7 +0,0 @@
-public class CtCI_Questions
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Testing");
-    }
-}
---
+++ src/IsUnique.java
@@ -0,0 +1,8 @@
+
+//Implement an algorithm to determine if a String has all unique characters.
+
+public class IsUnique {
+    boolean is_Unique(String s) {
+        
+    }
+}
