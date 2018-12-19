3249231f437984df7d63727c8efdefef10efc260
Andy <andyn11bdf@gmail.com>
2018-12-19 17:47:31 -0500

    Practicing reversing a String
    Signed-off-by: Andy <andyn11bdf@gmail.com>

---
+++ src/ReverseString.java
@@ -0,0 +1,54 @@
+/*
+(1) Reverse a String in-place.
+(2) Then, reverse certain words in a character array (also in-place)
+ */
+public class ReverseString {
+    //String
+    private static void reverse1(String s) {
+        char temp;
+        char[] charArray = s.toCharArray();
+        int size = charArray.length;
+
+        for (int i = 0; i <= (s.length() / 2); i++) {
+            temp = charArray[size - 1 - i]; //Create temporary storage character of last
+            charArray[size - 1 - i] = charArray[i]; //Move first to last
+            charArray[i] = temp; //Move last to first
+        }
+
+    }
+
+    //Character array with specified start/end indices
+    private static void reverse2(char[] array, int firstIndex, int lastIndex) {
+        char temp;
+        int size = lastIndex;
+
+        for (int i = firstIndex; i < array.length / 2; i++) {
+            temp = array[size - 1 - i];
+            array[size - 1 - i] = array[i];
+            array[i] = temp;
+        }
+
+    }
+
+   /* public static String reverseWords(char[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] == ' ')
+            {
+                reverse2(array,0,i);
+            }
+        }
+    }*/
+
+    public static void main(String[] args) {
+        String example = "This sentence should be reversed";
+        reverse1(example);
+
+        char[] test = example.toCharArray();
+        reverse2(test, 0, test.length);
+
+        char[] sentence = {'W', 'o', 'r', 'l', 'd', ' ', 'H', 'e', 'l', 'l', 'o'};
+
+    }
+}
