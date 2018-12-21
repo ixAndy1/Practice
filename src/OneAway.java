628f9f00d67a34c22cca9f73654bab9d65db1503
Andy <andyn11bdf@gmail.com>
2018-12-21 00:24:01 -0500

    Fix Line 71, Array out of bounds exception
    Signed-off-by: Andy <andyn11bdf@gmail.com>

--- src/OneAway.java
+++ src/OneAway.java
@@ -4,59 +4,87 @@ There are three types of edits that can be performed in strings:
 Given two strings, write a function to check if they are one (or zero) edits away.
  */
 
+import java.util.Arrays;
+
 public class OneAway {
     private static boolean check_One_Away(String s1, String s2) {
 
         if (s1.equalsIgnoreCase(s2)) //If equal (ignoring case differences)
         {
+            System.out.println("Strings are equal (ignoring case differences)");
             return true;
+
         } else if (Math.abs(s1.length() - s2.length()) > 1) //If difference in length > 1
         {
+            System.out.println("More than one away");
             return false;
-        } else if (s1.length() < s2.length()) // (1) Character Insertion
-        {
-            for (int i = 0; i < s2.length(); i++) {
-                char[] charArray = new char[s1.length()];
-
-                if (s1.charAt(i) == s2.charAt(i)) {
-                    charArray[i] = s1.charAt(i);
-                } else {
-                    charArray[i] = s2.charAt(i);
-                }
-            }
-        } else if (s1.length() > s2.length()) // (2) Character Delete
-        {
-            char[] charArray = new char[s1.length()];
-
-            for (int i = 0; i < s2.length(); i++) {
-                if (s1.charAt(i) == s2.charAt(i)) {
-                    charArray[i] = s1.charAt(i);
-                } else {
-                    charArray[i] = Character.MIN_VALUE; //Empty character
-                }
-            }
-
-        } else  // (3) Character Replacement
-        {
-            int difference = 0;
-
-            for (int i = 0; i < s1.length(); i++) {
-                if (s1.charAt(i) != s2.charAt(i)) {
-                    difference++;
-                }
-
-                if (difference > 1) {
-                    return false;
-                }
-            }
         }
 
-        return true;
+        int whichMethod = s1.length() - s2.length();
+
+        switch (whichMethod)
+        {
+            case 0: // (3) Character replacement
+            {
+                for (int i = 0; i < s1.length(); i++) {
+                    if (s1.charAt(i) != s2.charAt(i)) {
+                        s1 = s1.replace(s1.charAt(i), s2.charAt(i));
+                    }
+                }
+                System.out.println("String one: " + s1);
+                System.out.println("String two: " + s2);
+                System.out.println("Case 0");
+                return true;
+            }
+            case 1: // (2) Character Removal
+            {
+                char[] array = new char[s1.length()];
+                for (int i = 0; i < s2.length(); i++) {
+                    if (s1.charAt(i) == s2.charAt(i)) {
+                        array[i] = s1.charAt(i);
+                    } else {
+                        array[i] = s2.charAt(i);
+                    }
+                }
+                System.out.println("String one: " + Arrays.toString(array)); //Works, different format
+                System.out.println("String two: " + s2);
+                System.out.println("Case 1");
+                return true;
+            }
+
+            case -1: // (1) Character Insertion
+            {
+
+                char[] charArray = new char[s2.length()];
+
+                for (int i = 0; i < s2.length(); i++) {
+                    if (s1.charAt(i) == s2.charAt(i)) {
+                        charArray[i] = s1.charAt(i);
+                        System.out.println("No");
+
+                    } else {
+                        System.out.println("Hit");
+                    }
+
+                }
+
+
+                System.out.println("String one: " + Arrays.toString(charArray));
+                System.out.println("String two: " + s2);
+                System.out.println("Case -1");
+                return true;
+            }
+
+        }
+        return true; //Unused
     }
 
     public static void main(String[] args) {
-        String one = "pale";
-        String two = "pale";
+        String one = "pak";
+        String two = "pake";
+
+        System.out.println("Original one: " + one);
+        System.out.println("Original two: " + two);
 
         System.out.println(check_One_Away(one, two));
     }
