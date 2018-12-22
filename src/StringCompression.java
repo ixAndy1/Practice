c87f666f0b2d978abdc178e56c93eb10e13f725a
Andy <andyn11bdf@gmail.com>
2018-12-21 23:12:25 -0500

    Setting up String Compression
    Signed-off-by: Andy <andyn11bdf@gmail.com>

---
+++ src/StringCompression.java
@@ -0,0 +1,15 @@
+/*
+Implement a method to perform basic string compression
+using the counts of repeated characters
+ */
+public class StringCompression {
+    StringBuilder result = new StringBuilder();
+
+    public static StringBuilder compress(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
