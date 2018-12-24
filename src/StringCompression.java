d666595fe0651a01710bddf0bad6e717108cc741
Andy <andyn11bdf@gmail.com>
2018-12-24 17:08:54 -0500

    Works except for the last sequence of similar characters in a String
    Signed-off-by: Andy <andyn11bdf@gmail.com>

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -3,13 +3,28 @@ Implement a method to perform basic string compression
 using the counts of repeated characters
  */
 public class StringCompression {
-    StringBuilder result = new StringBuilder();
+    private static StringBuilder result = new StringBuilder();
 
-    public static StringBuilder compress(String s) {
+    private static StringBuilder compress(String s) {
 
+        int count = 1;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            } else {
+                result.append(s.charAt(i));
+                result.append(count);
+                count = 1;
+            }
+
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        String test = "aaa";
+
+        System.out.println(compress(test));
     }
 }
