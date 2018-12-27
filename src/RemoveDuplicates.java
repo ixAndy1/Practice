c9616af10d22da08784a602637b372d097345d31
Andy <andyn11bdf@gmail.com>
2018-12-27 18:16:39 -0500

    Setting up RemoveDuplicates
    Signed-off-by: Andy <andyn11bdf@gmail.com>

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,34 @@
+/*
+Remove duplicates from an unsorted (single) linked list
+ */
+
+public class RemoveDuplicates
+{
+    public static void main(String[] args)
+    {
+        Node head = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(2);
+        Node node5 = new Node(1);
+
+    }
+
+    void removeDuplicates(Node head)
+    {
+        
+    }
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node (int data)
+        {
+            this.data = data;
+        }
+    }
+
+
+}
