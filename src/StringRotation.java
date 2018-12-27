70e765627e2fb2d5d4dbc5d3dc8dabc091a0efdc
Andy <andyn11bdf@gmail.com>
2018-12-27 17:49:25 -0500

    Setting up StringRotation
    Signed-off-by: Andy <andyn11bdf@gmail.com>

---
+++ src/StringRotation.java
@@ -0,0 +1,16 @@
+/*
+Given two strings, write an algorithm to check if s2 is a rotation
+of s1 using only one call to isSubstring
+ */
+public class StringRotation
+{
+    private static boolean isSubstring(String s1, String s2)
+    {
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
